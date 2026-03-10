e0b42f94418507477e5c244dec2220828d94891c
uwe <uwe@uwes-mbp>
2026-03-10 12:37:55 +1100

    adding pg role

--- configuration/postgresql/initdb/001-chirpstack_extensions.sh
+++ configuration/postgresql/initdb/001-chirpstack_extensions.sh
@@ -4,4 +4,6 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$POSTGRES_DB" <<-EOSQL
     create extension pg_trgm;
     create extension hstore;
+    create role chirpstack with login password 'chirpstack';
+    create database chirpstack with owner chirpstack;
 EOSQL
