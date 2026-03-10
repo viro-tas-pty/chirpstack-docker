d9897ea0928bd7926d5a9399c3f6e4ef1503aa60
uwe <uwe@uwes-mbp>
2026-03-10 13:48:44 +1100

    adding new key adding plugin reduce pg version

--- configuration/postgresql/initdb/001-chirpstack_extensions.sh
+++ configuration/postgresql/initdb/001-chirpstack_extensions.sh
@@ -2,8 +2,9 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$POSTGRES_DB" <<-EOSQL
-    create extension pg_trgm;
-    create extension hstore;
+    create extension IF NOT EXISTS pg_trgm;
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+    create extension IF NOT EXISTS hstore;
     create role chirpstack with login password 'chirpstack';
     create database chirpstack with owner chirpstack;
 EOSQL
